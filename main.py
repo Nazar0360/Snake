3d7b43e095acef923b3dfbca288826e23ca76813
Nazar0360 <shmalkonazar0360@gmail.com>
2022-07-01 23:50:13 +0300

    The quality of the code improved, and better console output.

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ import numpy as np
 import os
 from typing import *
 
-pg.init()
-clock = pg.time.Clock()
-screen = pg.display.set_mode((1, 1))
-
 
 class Field:
-
     def __init__(self, shape: tuple[int, int]):
         self.__field = np.zeros(shape, dtype=np.int8)
 
@@ -19,8 +14,22 @@ class Field:
     def __repr__(self) -> str:
         return f'Field({self.__field.shape})'
 
-    def reload(self):
+    def clear(self):
         self.__field[:] = np.zeros(self.__field.shape, dtype=np.int8)
+
+    def print(self, pretty_print=True):
+        if pretty_print:
+            character_replacement = {0: '*', 1: '@', 2: '#'}
+            for index in range(self.__field.shape[0]):
+                for _index in range(self.__field.shape[1]):
+                    value = self.__field[index, _index]
+                    if value in character_replacement:
+                        print(character_replacement[value], end=' ')
+                    else:
+                        print(value, end=' ')
+                print()
+        else:
+            print(self)
 
     @property
     def field(self):
@@ -35,7 +44,7 @@ class Head:
     def __init__(self, _field: Field, pos):
         self.__field = _field
         self.__pos: np.array = np.array(pos)
-        self.__previous_pos = None
+        self.__previous_pos = np.array([-1, -1])
         self.__movement: np.array = np.zeros(2, dtype=np.int8)
         self.__is_alive = True
         self.__tail = Tail(self)
@@ -45,21 +54,29 @@ class Head:
         next_pos = self.pos + self.movement
         if not self.check_for_obstacle(next_pos) and self.is_alive and any(self.movement != 0):
             self.pos = self.pos + self.movement
-            if self.check_for_obstacle(next_pos):
-                self.__is_alive = False
-        self.__field.field[self.__pos[0], self.__pos[1]] = 1
+        elif self.check_for_obstacle(next_pos):
+            self.__is_alive = False
+        self.write_to_array()
+        self.__tail.write_to_array()
 
     def change_movement_direction(self, key: Literal[pg.K_UP, pg.K_DOWN, pg.K_LEFT, pg.K_RIGHT]):
         directions = {pg.K_UP: [-1, 0], pg.K_DOWN: [1, 0], pg.K_LEFT: [0, -1], pg.K_RIGHT: [0, 1]}
-        if key in directions.keys() and not all(self.__movement == -np.array(directions[key])):
+        if key in directions and not all(self.__movement == -np.array(directions[key])):
             self.movement = directions[key]
 
     def check_for_obstacle(self, pos: Sequence) -> bool:
         pos = np.array(pos)
-        return not all(pos + 1 <= self.field.field.shape) and all(pos >= 0)
+        return not (all(pos + 1 <= self.field.field.shape) and all(pos >= 0))
 
     def write_to_array(self):
         self.field.field[self.pos[0], self.pos[1]] = 1
+
+    def print_debug_info(self):
+        print(f'pos: {self.pos}\n'
+              f'previous_pos: {self.previous_pos}\n'
+              f'movement: {self.movement}\n'
+              f'is_alive: {self.is_alive}\n'
+              f'tail_elements_pos: {self.tail.tail_elements_pos}')
 
     @property
     def pos(self):
@@ -69,7 +86,7 @@ class Head:
     def pos(self, value):
         self.__previous_pos = self.pos
         self.__pos = np.array(value)
-        self.__tail.move()
+        self.__tail.update_pos()
 
     @property
     def movement(self):
@@ -91,74 +108,65 @@ class Head:
     def previous_pos(self):
         return self.__previous_pos
 
+    @property
+    def tail(self):
+        return self.__tail
+
 
 class Tail:
-    def __init__(self, next_part):
-        self.__pos = None
-        self.__previous_pos = None
-        self.__next_part: Tail | Head = next_part
-        self.__previous_part: Tail | None = None
-        part: Tail | Head = next_part
-        _id = 0
-        while not isinstance(part, Head):
-            part = part.next_part
-            _id += 1
-        self.__head: Head = part
-        self.__id = _id
+    def __init__(self, _head: Head):
+        self.__head = _head
+        self.__tail_elements_pos: np.array = np.array([[-1, -1]])
 
-    def add_new_tail(self):
-        last_part: Tail | None = self.__previous_part
-        if last_part is not None:
-            while True:
-                if last_part.__previous_part is None:
-                    break
-                last_part = last_part.__previous_part
-        if last_part is None:
-            last_part = self
-        with open('f.txt', 'w') as f:
-            f.write(f'{last_part=}, {last_part.__previous_part=}')
-        last_part.__previous_part = Tail(last_part)
+    def add_new_element(self, amount=1):
+        for _ in range(amount):
+            unprepared_array = np.append(self.__tail_elements_pos, [-1, -1])
+            self.__tail_elements_pos = unprepared_array.reshape((unprepared_array.shape[0] // 2, 2))
 
-    def move(self):
-        self.pos = self.__next_part.previous_pos
-        if self.__previous_part is not None:
-            self.__previous_part.move()
+    def update_pos(self):
+        if any(self.__head.pos != self.__tail_elements_pos[0]):
+            for index in range(len(self.__tail_elements_pos)):
+                if index != len(self.__tail_elements_pos) - 1:
+                    self.__tail_elements_pos[index] = self.__tail_elements_pos[index + 1]
+                else:
+                    self.__tail_elements_pos[index] = self.__head.previous_pos[:]
+        self.write_to_array()
+
+    def write_to_array(self):
+        for element_pos in self.__tail_elements_pos:
+            if all(element_pos != np.array([-1, -1])):
+                self.__head.field.field[element_pos[0], element_pos[1]] = 2
 
     @property
-    def next_part(self):
-        return self.__next_part
-
-    @property
-    def pos(self):
-        return self.__pos
-
-    @pos.setter
-    def pos(self, value):
-        self.__previous_pos = self.pos
-        self.__pos = value
-        if self.__pos is not None:
-            self.__head.field.field[self.pos[0], self.pos[1]] = 2
-
-    @property
-    def previous_pos(self):
-        return self.__previous_pos
+    def tail_elements_pos(self):
+        return self.__tail_elements_pos
 
 
-field = Field((21, 21))
-head = Head(field, (10, 10))
-time = 0
+def main():
+    pg.init()
+    clock = pg.time.Clock()
+    # the screen variable for a future
+    screen = pg.display.set_mode((1, 1))
 
-while True:
-    print(f'pos: {head.pos}\n'f'movement: {head.movement}\nis_alive: {head.is_alive}\n'
-          f'previous_pos: {head.previous_pos}')
-    for event in pg.event.get():
-        [exit() for _ in ' ' if event.type == pg.QUIT]
-        if event.type == pg.KEYDOWN:
-            head.change_movement_direction(event.key)
-    os.system('cls')
-    if time % 5 == 0:
-        field.reload()
-        head.move()
-    print(field)
-    clock.tick(60)
-    time += 1
+    field = Field((21, 21))
+    head = Head(field, (10, 10))
+    head.tail.add_new_element(3)
+    time = 0
+
+    while True:
+        head.print_debug_info()
+        for event in pg.event.get():
+            [exit() for _ in ' ' if event.type == pg.QUIT]
+            if event.type == pg.KEYDOWN:
+                head.change_movement_direction(event.key)
+        os.system('cls')
+        if time % 5 == 0:
+            field.clear()
+            head.move()
+        field.print()
+        clock.tick(60)
+        time += 1
+
+
+if __name__ == '__main__':
+    main()
